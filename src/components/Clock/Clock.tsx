f76ffa968bc6658f2ba05ae2e59c7c528b492b5f
Ty Eggen <tycho.eggen@gmail.com>
2022-05-04 16:14:43 -0700

    feat: clock time setting

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,24 +1,40 @@
+import stateMachine from 'pretty-state-machine'
 import React, { FC, useEffect, useState } from 'react'
-import styles from './Clock.module.css'
 
 import { ClockProps } from './Clock.types'
 
-const renderTime = () => {
+import styles from './Clock.module.css'
+
+const renderTime = (clockMode:number) => {
   const date = new Date()
 
-  const hours = ('' + date.getHours()).padStart(2, '0')
+  const hours = ('' + ((clockMode === 24) ? date.getHours() : (date.getHours() % 12))).padStart(2, '0')
   const minutes = ('' + date.getMinutes()).padStart(2, '0')
   const seconds = ('' + date.getSeconds()).padStart(2, '0')
 
-  return `${hours}:${minutes}:${seconds}`
+  return (clockMode === 24) ? `${hours}:${minutes}:${seconds}` : `${hours}:${minutes}:${seconds} ${date.getHours() > 12 ? 'PM' : 'AM'}`
 }
 
 const Clock: FC<ClockProps> = () => {
-  const [clock, setClock] = useState(renderTime())
+  const [clockMode, setClockMode] = useState(24)
+  const [clock, setClock] = useState(renderTime(clockMode))
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const setClockModeHandler = (update: { clockMode: number }) => {
+    setClockMode(update.clockMode)
+    setClock(renderTime(update.clockMode))
+  }
+
+  // @ts-ignore
+  useEffect(() => {
+    stateMachine.sub('clockMode', setClockModeHandler)
+
+    return () => stateMachine.unsub('clockMode', setClockModeHandler)
+  }, [setClockModeHandler])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setClock(renderTime())
+      setClock(renderTime(clockMode))
     }, 1000)
 
     return () => {
