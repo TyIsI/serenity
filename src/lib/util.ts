aca92c752058241fcc36644038dff062f45451d2
Ty Eggen <tycho.eggen@gmail.com>
2022-06-21 18:43:53 +0000

    feat(util): new getRandomId and getDebugger functions

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,8 @@
-export const reorder = (list: any|any[], startIndex: number, endIndex: number): unknown[] => {
+import Debug from 'debug'
+
+let debug: Debug.Debugger | null = null
+
+export const reorder = (list: any | any[], startIndex: number, endIndex: number): unknown[] => {
   const result = Array.from(list)
 
   const [removed] = result.splice(startIndex, 1)
@@ -6,4 +10,16 @@ export const reorder = (list: any|any[], startIndex: number, endIndex: number): 
   result.splice(endIndex, 0, removed)
 
   return result
+}
+
+export const getRandomId = () => {
+  return '' + Math.random().toString(36).substring(2, 9)
+}
+
+export const getDebugger = (namespace: string) => {
+  if (debug === null) {
+    debug = Debug('serenity-dashboard')
+  }
+
+  return debug.extend(namespace)
 }
